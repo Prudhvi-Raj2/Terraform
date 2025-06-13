99b69344d9e20c6d2646d4e9bd459667765bbd59
prudh <prudhhviraj2@gmail.com>
2025-06-13 06:08:48 +0530

    adding loops concepts in that only count has been added

---
+++ count/variables.tf
@@ -0,0 +1,13 @@
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_Id" {
+  default = "Z04650642PDDCGVZMN5D"
+
+}
+
+variable "domain_name" {
+  default = "kambalas.shop"
+
+}
